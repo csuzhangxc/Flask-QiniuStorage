b76c3330fda609ea990c1af413f04e796db59ba3
csuzhangxc <csuzhangxc@gmail.com>
2017-02-11 13:09:10 +0800

    urljoin兼容Python3

--- flask_qiniustorage.py
+++ flask_qiniustorage.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from urlparse import urljoin
+try:
+    from urlparse import urljoin
+except ImportError:
+    from urllib.parse import urljoin
 import qiniu as QiniuClass
 
 
